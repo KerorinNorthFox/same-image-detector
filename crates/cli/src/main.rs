2538c77d5e47897a986e4108459f3ed5b50496b2
kerorinnf <kerorinnf@gmail.com>
2026-06-17 20:57:57 +0900

    feat: cliツールにコマンドライン引数からパスを取得するコードを実装

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Arg, Command};
+use std::path::Path;
+// use compare;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("same-image-detector")
+        .arg(Arg::new("base_path").help("").required(true).index(1))
+        .arg(Arg::new("target_path").help("").required(true).index(2))
+        .get_matches();
+
+    let base_path = match matches.get_one::<String>("base_path") {
+        Some(base_path_arg) => {
+            dbg!(base_path_arg);
+            Path::new(base_path_arg)
+        }
+        None => panic!(),
+    };
+    let target_path = match matches.get_one::<String>("target_path") {
+        Some(target_path_arg) => {
+            dbg!(target_path_arg);
+            Path::new(target_path_arg)
+        }
+        None => panic!(),
+    };
 }
